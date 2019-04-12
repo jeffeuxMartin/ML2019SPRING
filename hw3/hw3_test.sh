6f34a7dbca257bd4f97f62987852f6b6a3a1c4fb
Jeff Chen <jeffkevin3@gmail.com>
2019-04-12 19:13:20 +0800

    "updated..."

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -3,11 +3,11 @@ mkdir res
 
 mkdir weights
 cd weights
-curl -O https://github.com/jeffeuxMartin/ML2019SPRING/releases/download/0.0.0/model_best_0408_010003.h5
-curl -O https://github.com/jeffeuxMartin/ML2019SPRING/releases/download/0.0.0/model_best_0408_045244.h5
-curl -O https://github.com/jeffeuxMartin/ML2019SPRING/releases/download/0.0.0/model_best_0409_044115.h5
-curl -O https://github.com/jeffeuxMartin/ML2019SPRING/releases/download/0.0.0/model_best_0410_033221.h5
-curl -O https://github.com/jeffeuxMartin/ML2019SPRING/releases/download/0.0.0/model_best_0410_034513.h5
+curl -O https://github.com/jeffeuxMartin/ML-Files/releases/download/0.1.0/model_best_0408_010003.h5
+curl -O https://github.com/jeffeuxMartin/ML-Files/releases/download/0.1.0/model_best_0408_045244.h5
+curl -O https://github.com/jeffeuxMartin/ML-Files/releases/download/0.1.0/model_best_0409_044115.h5
+curl -O https://github.com/jeffeuxMartin/ML-Files/releases/download/0.1.0/model_best_0410_033221.h5
+curl -O https://github.com/jeffeuxMartin/ML-Files/releases/download/0.1.0/model_best_0410_034513.h5
 cd ..
 
 python3 testing.py --testage "$1" --models weights --results res
