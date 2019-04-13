2e9b0d5066d28f034e2a71414f1c0904251e0afb
Jeff Chen <jeffkevin3@gmail.com>
2019-04-13 23:58:37 +0800

    "updated..."

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -100,3 +100,88 @@ for nindx, idx in enumerate(fine_pic):
     plt.imshow(image.astype('uint8'))
     plt.savefig(sys.argv[2] + '/fig3_{}.jpg'.format(nindx))
 #     print('fig3_{}.jpg'.format(nindx))
+
+import time
+s_time = time.time()
+X_train = x
+# util function to convert a tensor into a valid image
+def deprocess_image(x):
+    # normalize tensor: center on 0., ensure std is 0.1
+    x -= x.mean()
+    x /= (x.std() + 1e-5)
+    x *= 0.1
+
+    # clip to [0, 1]
+    x += 0.5
+    x = np.clip(x, 0, 1)
+
+    # convert to RGB array
+    x *= 255
+    #x = x.transpose((1, 2, 0))
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+def vis_img_in_filter(img, 
+                      layer_name):
+    img = np.array(img).reshape(-1, 48, 48, 1).astype(np.float64)[0].reshape(1, 48, 48, 1)
+#     print(img.shape)
+    layer_output = layer_dict[layer_name].output
+    img_ascs = list()
+    for filter_index in range(layer_output.shape[3]):
+        # build a loss function that maximizes the activation
+        # of the nth filter of the layer considered
+        loss = K.mean(layer_output[:, :, :, filter_index])
+
+        # compute the gradient of the input picture wrt this loss
+        grads = K.gradients(loss, model.input)[0]
+
+        # normalization trick: we normalize the gradient
+        grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
+
+        # this function returns the loss and grads given the input picture
+        iterate = K.function([model.input], [loss, grads])
+
+        # step size for gradient ascent
+        step = 5.
+
+#         img_asc = (np.random.random(img.shape) * 255.0).astype(int).astype(float)
+        img_asc = (np.zeros_like(img)).astype(np.int32).astype(np.float64)
+        
+        for i in range(20):
+            loss_value, grads_value = iterate([img_asc])
+            img_asc += grads_value * step
+
+        img_asc = img_asc[0]        
+        img_ascs.append(deprocess_image(img_asc).reshape((48, 48)))
+
+    plot_x, plot_y = 8, 8
+    fig, ax = plt.subplots(plot_x, plot_y, figsize = (12, 12))
+    fig.suptitle('Input image and %s filters' % (layer_name,))
+    fig.tight_layout(rect = [0, 0, 1, 1], pad=0)
+    for (x, y) in [(i, j) for i in range(plot_x) for j in range(plot_y)]:
+        ax[x, y].imshow(img_ascs[x * plot_y + y], cmap = 'gray')#, interpolation="nearest")
+        ax[x, y].axis('off')
+    fig.savefig('fig2_1.jpg')
+    return img_ascs
+    
+def vis_oup_in_filter(model, img, layer_name):
+    img = img.reshape(-1, 48, 48, 1)[0].reshape(1, 48, 48, 1)
+    get_outputs = K.function([model.layers[0].input],
+                            [layer_dict[layer_name].output])
+    kernels_output = get_outputs([img])[0]
+    kernels_output = kernels_output.transpose(3, 0, 1, 2)
+    channel, img_x, img_y, batch_sz= kernels_output.shape
+    plot_x, plot_y = channel // 8, 8
+    fig, ax = plt.subplots(plot_x, plot_y, figsize = (12, 12))
+    fig.suptitle('Output images of %s filters' % (layer_name,))
+    fig.tight_layout(rect = [0, 0, 1, 1], pad=0)
+    for (x, y) in [(i, j) for i in range(plot_x) for j in range(plot_y)]:
+        ax[x, y].imshow(kernels_output[x * plot_y + y].reshape(img_x, img_y), cmap = 'gray')
+        ax[x, y].axis('off')
+    fig.savefig('fig2_2.jpg')
+    return kernels_output
+
+img_ascs = vis_img_in_filter(x[fine_pic[1]], 'conv2d_1')
+# print(time.time() - s_time)
+kernels_output = vis_oup_in_filter(model, x[fine_pic[1]], 'conv2d_1')
+# print(time.time() - s_time)
