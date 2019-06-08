7af21d84f2ce752ec3e2f614bfde46500c4b939a
Jeff Chen <jeffkevin3@gmail.com>
2019-06-09 00:48:15 +0800

    20190609_004815...

--- hw7/cluster.sh
+++ hw7/cluster.sh
@@ -1,1 +1,3 @@
-python3 reproduce.py $1 $2 $3 Bestweight_190521_065402.h5 
+curl https://www.dropbox.com/s/4n8nadt58hikraz/Bestweight_190521_065402.h5?dl=1 -O -J -L
+
+python3 reproduce.py $1 $2 $3 Bestweight_190521_065402.h5
